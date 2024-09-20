dd2594f28d7864789b9dda482a20e6b964cf158e
max <gusevmv@icloud.com>
2024-09-20 11:12:37 +0300

    initial commit

---
+++ game1.py
@@ -0,0 +1,8 @@
+import numpy as np
+def random_predict(number:int=1) -> int:
+    Args:
+        number (int, optionl): Загаданное число. Defaults to 1.
+        
+        Returns:
+            int: Число попыток
+        """    
